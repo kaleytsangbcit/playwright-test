bcafc809842a89add29a376c50e01e8cd4b1ab5d
kaleytsangbcit <ktsang38@my.bcit.ca>
2023-01-24 10:46:13 -0800

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+export default function About(){
+    return (
+        <>This is About Page
+        
+        <div>
+          <Link href="/">Go Back</Link>
+        </div>
+        
+        </>
+    )
+}
